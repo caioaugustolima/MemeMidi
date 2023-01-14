8ec4dc3d4ab0ea2cb07c1e8c2c4cc068ceb553cf
Caio Lima <50187646+caioaugustolima@users.noreply.github.com>
2023-01-14 20:01:14 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function tocaSom (idElementoAudio) {
+
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
+    
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    //template string
+    const idAudio = `#som_${instrumento}`;
+
+    //console.log(idAudio)
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    };
+
+}
