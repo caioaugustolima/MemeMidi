805f005a65a229614449b594974ca7f49de1bfcf
Caio Lima <50187646+caioaugustolima@users.noreply.github.com>
2023-01-15 20:23:09 -0300

    Update main.js
     - Atualização para que a animação de ativação dos botões funcione também com a tecla enter e espaço sem se replicar quando usamos o tab.
    
     - Refinamento do código para impedir que função usada no play do áudio (crucial para o funcionamento da tela) não recebe como parâmetro nenhum outro elemento no console que não seja um arquivo de áudio.

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-function tocaSom (idElementoAudio) {
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
 
-    document.querySelector(idElementoAudio).play();
-}
+    if (elemento != null && elemento.localName === 'audio'){        
+            elemento.play();        
+    }else{
+        alert('Elemento não encontrado');
+    }
+
+ }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
@@ -9,15 +15,27 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
     
     const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
+    const som = tecla.classList[1];
 
     //template string
-    const idAudio = `#som_${instrumento}`;
+    const idAudio = `#som_${som}`;
 
     //console.log(idAudio)
 
     tecla.onclick = function(){
         tocaSom(idAudio);
-    };
+    }
+
+    tecla.onkeydown = function (evento){
+        
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
+    }
 
 }
